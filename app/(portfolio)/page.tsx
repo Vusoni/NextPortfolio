7bb4f3e2710b9a4b3644ae3d05c1366cdac50144
Vusoni <valoenu.contact@gmail.com>
2025-11-17 23:07:06 +0100

    First Day

---
+++ app/(portfolio)/page.tsx
@@ -0,0 +1,11 @@
+// Import
+import PortfolioContent from "@/components/PortfolioContent";
+
+// Home Page
+export default async function Home() {
+  return (
+    <main className="min-h-screen">
+      <PortfolioContent />
+    </main>
+  );
+}
---
+++ components/PortfolioContent.tsx
@@ -0,0 +1,36 @@
+// Imports
+import {
+  AboutSection,
+  AchievementsSection,
+  BlogSection,
+  CertificationsSection,
+  ContactSection,
+  EducationSection,
+  ExperienceSection,
+  HeroSection,
+  ProjectsSection,
+  ServicesSection,
+  SkillsSection,
+  TestimonialsSection,
+} from "@/components/sections";
+
+async function PortfolioContent() {
+  return (
+    <>
+      <HeroSection />
+      <AboutSection />
+      <TestimonialsSection />
+      <SkillsSection />
+      <ExperienceSection />
+      <EducationSection />
+      <ProjectsSection />
+      <CertificationsSection />
+      <AchievementsSection />
+      <ServicesSection />
+      <BlogSection />
+      <ContactSection />
+    </>
+  );
+}
+
+export default PortfolioContent;
